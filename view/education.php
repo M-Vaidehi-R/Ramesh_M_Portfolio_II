1fec7f4d156ce33754b358934c23bc7b05655063
Mamatha Vaidehi Ramesh <m_ramesh150220@fanshaweonline.ca>
2023-03-21 19:25:32 -0400

    css in progress

--- view/education.php
+++ view/education.php
@@ -1,5 +1,31 @@
 <?php 
 
-echo "education";
+//echo "education";
 
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../public/css/main.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600&display=swap" rel="stylesheet">
+    <title>Education</title>
+</head>
+<body>
+    <div class="page-titles">
+    <h2>About Me </h2>
+    <h3>Education</h3>
+    </div>
+
+    <div class="two-pictures">
+
+    <img src="../public/images/education2.svg" alt="education 1 ">
+    <img src="../public/images/education1.svg" alt="education 2">
+</div>
+</body>
+</html>
--- view/experience.php
+++ view/experience.php
@@ -1,5 +1,33 @@
 <?php 
 
-echo "experience";
+//echo "experience";
 
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Experience</title>
+    <link rel="stylesheet" href="../public/css/main.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600&display=swap" rel="stylesheet">
+</head>
+<body>
+
+    <div class="page-titles">
+    <h2>AboutMe</h2>
+    <h3>Experience</h3>
+    </div>
+
+
+<div class="two-pictures">
+
+    <img src="../public/images/coder.svg" alt="coder 1 ">
+    <img src="../public/images/coder-2.svg" alt="coder 2">
+</div>
+</body>
+</html>
