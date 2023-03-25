fe1b14033bc346819aae7225de7fb152a85573cc
Mamatha Vaidehi Ramesh <m_ramesh150220@fanshaweonline.ca>
2023-03-25 07:12:42 -0400

    taking shape, added a couple of things

--- view/display_all_ports.php
+++ view/display_all_ports.php
@@ -1,12 +1,19 @@
 <div class="user_list">
 	<div class="user_list_box">
-		<ul>
+		<ul class="project-thumb">
 			<?php
 			
 			for($i = 0; $i < $rows; $i++) { 
 				echo '<a class="" href="view/projects.php?id='.$ports[$i]->id.'">
-						<!--<li>'.$ports[$i]->title.'</li>
-						<li><img src="./public/images/thumbnails/'.$ports[$i]->thumbnails.'.svg" alt="thumbnails"></li>
+						<div class="project-thumb-box">
+								<li>
+									<img src="./public/images/thumbnails/'.$ports[$i]->thumbnail.'" alt="'.$ports[$i]->title.'">
+
+									<div class="image-text">
+							 			'.$ports[$i]->thumb_text.'
+								  </div>
+								</li>
+						</div>
 					</a>';
 			}
 			?>
